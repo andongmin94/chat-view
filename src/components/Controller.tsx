3322420d9266436d4bffb51dc489ceb9959cb225
안동민 <andongmin94@gmail.com>
2024-08-29 21:11:28 +0900

    prepare release

--- src/components/Controller.tsx
+++ src/components/Controller.tsx
@@ -41,6 +41,32 @@ export default function Component() {
   const [isDialogOpen, setIsDialogOpen] = React.useState(false);
   const [urlError, setUrlError] = React.useState<string | null>(null);
   const [isFirstRun, setIsFirstRun] = React.useState(true);
+  const [isUpdateDialogOpen, setIsUpdateDialogOpen] = React.useState(false);
+
+  React.useEffect(() => {
+    electron.on("update_available", () => {
+      setIsUpdateDialogOpen(true);
+    });
+
+    electron.on("update_downloaded", () => {
+      const response = confirm(
+        "업데이트가 다운로드되었습니다. 지금 설치하시겠습니까?",
+      );
+      if (response) {
+        electron.send("install_update");
+      }
+    });
+
+    return () => {
+      electron.removeAllListeners("update_available");
+      electron.removeAllListeners("update_downloaded");
+    };
+  }, []);
+
+  const handleDownloadUpdate = () => {
+    electron.send("download_update");
+    setIsUpdateDialogOpen(false);
+  };
 
   React.useEffect(() => {
     const fetchInitialState = async () => {
@@ -94,81 +120,115 @@ export default function Component() {
   return (
     <div>
       <TitleBar />
-      <CardHeader className="flex justify-center">
-        <CardTitle>채팅 오버레이 제어판</CardTitle>
-        <CardDescription>채팅 오버레이 설정을 관리합니다</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <div className="grid w-full items-center gap-4">
-          <Dialog
-            open={isDialogOpen}
-            onOpenChange={(e) => {
-              setIsDialogOpen(e);
-              electron.send("reInput");
-            }}
-          >
-            <DialogTrigger asChild>
-              <Button variant="outline">
-                {isFirstRun ? "URL 입력하기" : "URL 다시 입력하기"}
+      {isUpdateDialogOpen ? (
+        <Dialog open={isUpdateDialogOpen} onOpenChange={setIsUpdateDialogOpen}>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>업데이트 알림</DialogTitle>
+              <DialogDescription>
+                업데이트가 가능합니다. 다운로드하시겠습니까?
+              </DialogDescription>
+            </DialogHeader>
+            <DialogFooter className="grid grid-cols-4">
+              <Button
+                onClick={handleDownloadUpdate}
+                className="bg-green-500 text-white hover:bg-green-600"
+              >
+                다운로드
               </Button>
-            </DialogTrigger>
-            <DialogContent className="mt-5 max-w-[355px] rounded-md p-4">
-              <DialogHeader>
-                <DialogTitle>채팅 URL 설정</DialogTitle>
-                <DialogDescription>
-                  채팅 오버레이에 표시할 URL을 입력하세요.
-                </DialogDescription>
-              </DialogHeader>
-              <div className="grid">
-                <div className="grid items-center">
-                  <Input id="url" value={url} onChange={handleUrlChange} />
+              <Button
+                variant="outline"
+                onClick={() => setIsUpdateDialogOpen(false)}
+                className="col-start-4 border-gray-300 text-gray-700 hover:bg-gray-100"
+              >
+                취소
+              </Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      ) : (
+        <>
+          <CardHeader className="flex justify-center">
+            <CardTitle>채팅 오버레이 제어판</CardTitle>
+            <CardDescription>채팅 오버레이 설정을 관리합니다</CardDescription>
+          </CardHeader>
+          <CardContent>
+            <div className="grid w-full items-center gap-4">
+              <Dialog
+                open={isDialogOpen}
+                onOpenChange={(e) => {
+                  setIsDialogOpen(e);
+                  electron.send("reInput");
+                }}
+              >
+                <DialogTrigger asChild>
+                  <Button variant="outline">
+                    {isFirstRun ? "URL 입력하기" : "URL 다시 입력하기"}
+                  </Button>
+                </DialogTrigger>
+                <DialogContent className="mt-5 max-w-[355px] rounded-md p-4">
+                  <DialogHeader>
+                    <DialogTitle>채팅 URL 설정</DialogTitle>
+                    <DialogDescription>
+                      채팅 오버레이에 표시할 URL을 입력하세요.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid">
+                    <div className="grid items-center">
+                      <Input id="url" value={url} onChange={handleUrlChange} />
+                    </div>
+                    {urlError && (
+                      <p className="text-sm text-red-500">{urlError}</p>
+                    )}
+                  </div>
+                  <DialogFooter className="grid grid-cols-4">
+                    <Button
+                      onClick={handleApply}
+                      disabled={!!urlError}
+                      className="col-start-4 text-sm"
+                    >
+                      적용
+                    </Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+              <div className="mt-2 flex items-center justify-between">
+                <div className="flex items-center">
+                  <Label htmlFor="fixed-mode" className="mr-2">
+                    고정 활성화
+                  </Label>
+                  <Switch
+                    id="fixed-mode"
+                    checked={isFixed}
+                    onCheckedChange={handleFixedToggle}
+                  />
                 </div>
-                {urlError && <p className="text-sm text-red-500">{urlError}</p>}
+                <AlertDialog>
+                  <AlertDialogTrigger asChild>
+                    <Button variant="destructive">리셋</Button>
+                  </AlertDialogTrigger>
+                  <AlertDialogContent>
+                    <AlertDialogHeader>
+                      <AlertDialogTitle>
+                        정말로 리셋하시겠습니까?
+                      </AlertDialogTitle>
+                      <AlertDialogDescription>
+                        이 작업은 되돌릴 수 없습니다. 모든 설정이 초기화됩니다.
+                      </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                      <AlertDialogCancel>취소</AlertDialogCancel>
+                      <AlertDialogAction onClick={handleReset}>
+                        리셋
+                      </AlertDialogAction>
+                    </AlertDialogFooter>
+                  </AlertDialogContent>
+                </AlertDialog>
               </div>
-              <DialogFooter className="grid grid-cols-4">
-                <Button
-                  onClick={handleApply}
-                  disabled={!!urlError}
-                  className="col-start-4 text-sm"
-                >
-                  적용
-                </Button>
-              </DialogFooter>
-            </DialogContent>
-          </Dialog>
-          <div className="mt-2 flex items-center justify-between">
-            <div className="flex items-center">
-              <Label htmlFor="fixed-mode" className="mr-2">
-                고정 활성화
-              </Label>
-              <Switch
-                id="fixed-mode"
-                checked={isFixed}
-                onCheckedChange={handleFixedToggle}
-              />
             </div>
-            <AlertDialog>
-              <AlertDialogTrigger asChild>
-                <Button variant="destructive">리셋</Button>
-              </AlertDialogTrigger>
-              <AlertDialogContent>
-                <AlertDialogHeader>
-                  <AlertDialogTitle>정말로 리셋하시겠습니까?</AlertDialogTitle>
-                  <AlertDialogDescription>
-                    이 작업은 되돌릴 수 없습니다. 모든 설정이 초기화됩니다.
-                  </AlertDialogDescription>
-                </AlertDialogHeader>
-                <AlertDialogFooter>
-                  <AlertDialogCancel>취소</AlertDialogCancel>
-                  <AlertDialogAction onClick={handleReset}>
-                    리셋
-                  </AlertDialogAction>
-                </AlertDialogFooter>
-              </AlertDialogContent>
-            </AlertDialog>
-          </div>
-        </div>
-      </CardContent>
+          </CardContent>
+        </>
+      )}
     </div>
   );
 }

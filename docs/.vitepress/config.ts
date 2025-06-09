12a3df6a36cbed20aea6e671768c65e7fe466df4
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-06-09 12:35:49 +0900

    docs: 문서 정리 docs package.json

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,101 @@
+import { defineConfig } from "vitepress";
+import { transformerTwoslash } from "@shikijs/vitepress-twoslash";
+import { buildEnd } from "./buildEnd.config";
+
+const ogDescription = "GUI Library for Desktop App Development";
+const ogImage = "https://chat-view.andongmin.com/chat-view.svg";
+const ogTitle = "챗뷰";
+const ogUrl = "https://chat-view.andongmin.com";
+
+export default defineConfig({
+  title: "챗뷰",
+  description: "GUI Library for Desktop App Development",
+
+  head: [
+    ["link", { rel: "icon", type: "image/svg+xml", href: "/chat-view.svg" }],
+    [
+      "link",
+      { rel: "alternate", type: "application/rss+xml", href: "/blog.rss" },
+    ],
+    ["link", { rel: "organization", href: "https://github.com/andongmin94" }],
+    ["meta", { property: "og:type", content: "website" }],
+    ["meta", { property: "og:title", content: ogTitle }],
+    ["meta", { property: "og:image", content: ogImage }],
+    ["meta", { property: "og:url", content: ogUrl }],
+    ["meta", { property: "og:description", content: ogDescription }],
+    ["meta", { name: "theme-color", content: "#646cff" }],
+    [
+      "script",
+      {
+        src: "https://cdn.usefathom.com/script.js",
+        "data-site": "CBDFBSLI",
+        "data-spa": "auto",
+        defer: "",
+      },
+    ],
+  ],
+
+  themeConfig: {
+    logo: "/chat-view.svg",
+
+    editLink: {
+      pattern: "https://github.com/andongmin94/chat-view/edit/main/docs",
+      text: "Suggest changes to this page",
+    },
+
+    footer: {
+      message: `Released under the EULA License`,
+      copyright: "Copyright © 2024 안동민",
+    },
+
+    nav: [
+      { text: "챗뷰 가이드", link: "/guide", activeMatch: "/guide" },
+      { text: "챗뷰 개발자", link: "/maintainer" },
+    ],
+
+    sidebar: {
+      "/guide/": [
+        {
+          text: "챗뷰 가이드",
+          items: [
+            {
+              text: "챗뷰 시작하기",
+              link: "/guide/",
+            },
+            {
+              text: "숲 설정",
+              link: "/guide/soop",
+            },
+            {
+              text: "치지직 설정",
+              link: "/guide/chzzk",
+            },
+            {
+              text: "아프리카도우미 설정",
+              link: "/guide/afhp",
+            },
+          ],
+        }
+      ],
+    },
+
+    outline: {
+      level: [2, 3],
+    },
+  },
+  transformPageData(pageData) {
+    const canonicalUrl = `${ogUrl}/${pageData.relativePath}`
+      .replace(/\/index\.md$/, "/")
+      .replace(/\.md$/, "/");
+    pageData.frontmatter.head ??= [];
+    pageData.frontmatter.head.unshift([
+      "link",
+      { rel: "canonical", href: canonicalUrl },
+    ]);
+    return pageData;
+  },
+  markdown: {
+    codeTransformers: [transformerTwoslash()],
+  },
+  buildEnd,
+});

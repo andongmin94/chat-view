d114ee2c53bda4f3c928b2d2697b31587c873e94
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-08-13 00:27:45 +0900

    build: 릴리즈 전 정리 packages package.json

--- packages/src/electron/func.ts
+++ packages/src/electron/func.ts
@@ -1,14 +1,12 @@
+import { store } from "./main.js";
 import { getOverlayWindow } from "./window.js";
-import { getStore } from "./main.js";
 
-// const overlayWindow = getOverlayWindow(); // 이 줄을 제거합니다.
-
-export function updateFixedMode(isFixed:any) {
+export function updateFixedMode(isFixed: any) {
   const overlayWindow = getOverlayWindow(); // 함수 내부에서 직접 호출합니다.
   if (overlayWindow && !overlayWindow.isDestroyed()) {
     overlayWindow.setAlwaysOnTop(isFixed);
-    overlayWindow.setIgnoreMouseEvents(isFixed, {forward: true});
-    overlayWindow.webContents.send('update-style', isFixed);
+    overlayWindow.setIgnoreMouseEvents(isFixed, { forward: true });
+    overlayWindow.webContents.send("update-style", isFixed);
   }
 }
 
@@ -16,6 +14,6 @@ export function saveBounds() {
   const overlayWindow = getOverlayWindow(); // 함수 내부에서 직접 호출합니다.
   if (overlayWindow && !overlayWindow.isDestroyed()) {
     const bounds = overlayWindow.getBounds();
-    (getStore() as any).set('overlayWindowBounds', bounds);
+    (store() as any).set("overlayWindowBounds", bounds);
   }
 }

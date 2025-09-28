999d6f9dbf7c1ba7f8920bf19c650debd11cb0ac
안동민 <110483588+andongmin94@users.noreply.github.com>
2025-09-28 21:52:54 +0900

    build: 릴리즈 전 정리 packages package.json

--- docs/.vitepress/buildEnd.config.ts
+++ docs/.vitepress/buildEnd.config.ts
@@ -13,8 +13,8 @@ export const buildEnd = async (config: SiteConfig) => {
     id: blogUrl,
     link: blogUrl,
     language: "ko",
-    image: "https://chat-view.andongmin.com/logo.png",
-    favicon: "https://chat-view.andongmin.com/logo.png",
+    image: "https://chat-view.andongmin.com/logo.svg",
+    favicon: "https://chat-view.andongmin.com/logo.svg",
     copyright: "Copyright © 2025 andongmin",
   });
 

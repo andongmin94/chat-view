0c489dfa692dc50cacf3a208788f89dc0af31c36
안동민 <110483588+andongmin94@users.noreply.github.com>
2026-01-29 15:26:24 +0900

    build: 패키지 설정 정리 packages package.json

--- packages/src/electron/preload.ts
+++ packages/src/electron/preload.ts
@@ -2,8 +2,9 @@ const { contextBridge, ipcRenderer } = require("electron");
 
 contextBridge.exposeInMainWorld("electron", {
   send: (channel: any, data: any) => ipcRenderer.send(channel, data),
+  invoke: (channel: any, data: any) => ipcRenderer.invoke(channel, data),
   on: (channel: any, func: (...args: any[]) => any) =>
-    ipcRenderer.on(channel, (event: any, ...args: any[]) => func(...args)),
+    ipcRenderer.on(channel, (_event: any, ...args: any[]) => func(...args)),
   get: (key: any) => ipcRenderer.invoke("get-value", key),
   removeListener: (channel: any, func: any) =>
     ipcRenderer.removeListener(channel, func),
